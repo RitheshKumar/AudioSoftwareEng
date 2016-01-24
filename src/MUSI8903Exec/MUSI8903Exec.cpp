7a737dafeeb112d6164390a410f7b3718628e1a5
Liang_Tang <ltangmt@gmail.com>
2016-01-24 18:38:27 -0500

    1, implement reading audio file in the main function

--- src/MUSI8903Exec/MUSI8903Exec.cpp
+++ src/MUSI8903Exec/MUSI8903Exec.cpp
@@ -27,29 +27,66 @@ int main(int argc, char* argv[])
 
     CAudioFileIf            *phAudioFile        = 0;
 
+    enum eFilterTypes {
+        FIRCombFilter = 1,
+        IIRCombFilter = 2
+    };
+    
+    int iTypeOfFilter = 0;
+    
+    float fDelayInSec = 0.f;
+    
     showClInfo ();
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-
+//    if (argc <= 0 || argc > 10) {
+//        std::cerr << "The arguments are not properly provided" << std::endl;
+//    } else {
+//        sInputFilePath = argv[0];
+//        iTypeOfFilter = eFilterTypes.argv[1];
+//        fDelayInSec = (float)argv[2];
+//    }
+    
+    
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
-
+    CAudioFileIf::FileIoType_t fileType = CAudioFileIf::kFileRead;
+    CAudioFileIf::create(phAudioFile);
+    
+    phAudioFile -> openFile(sInputFilePath, fileType);
+    
+    CAudioFileIf::FileSpec_t fileSpec;
+    phAudioFile -> getFileSpec(fileSpec);
+    int numChannels = fileSpec.iNumChannels;
+    
+    phAudioFile -> getLength(iInFileLength);
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
+    ppfAudioData = new float* [numChannels];
+    
+    for (int channel = 0; channel < numChannels; channel++) {
 
+    }
+    
+    
     // get audio data
-
-    //////////////////////////////////////////////////////////////////////////////
-    // get audio info and print it to stdout
+    
 
     //////////////////////////////////////////////////////////////////////////////
     // do processing
-    cout << "Hello there!" << endl << endl;
+    
+    cout << "Processing is done!" << endl << endl;
 
     //////////////////////////////////////////////////////////////////////////////
     // clean-up
-
+    for (int channel = 0; channel < numChannels; channel++) {
+        delete ppfAudioData[channel];
+    }
+    delete ppfAudioData;
+    
+    CAudioFileIf::destroy(phAudioFile);
+    
     return 0;
     
 }
